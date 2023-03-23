d6a0c937ca20a90cceeab0ec498fe15d5919b7e7
ArthurBugan <arthurbugan@gmail.com>
2023-03-23 11:33:51 -0300

    initialize project and setup dependencies

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Default() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-4xl mb-5">Hello world!</Text>
+    </View>
+  );
+}
